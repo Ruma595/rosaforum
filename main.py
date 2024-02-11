8c663d09a7aa2c7c6b029e705cea1770c5ad9b06
BuildTools <unconfigured@null.spigotmc.org>
2024-02-11 12:21:23 +0100

    > Forum back, front

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 from flask import Flask, render_template, redirect, url_for, session, request
 import sqlite3
 import secrets
+import datetime
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "123456"
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def home_page():
     req = request.args.get("stat")
     if req == "login":
         return render_template("index.html", login=True)
     elif req == "signin":
         return render_template("index.html", signin=True)
-    return render_template("index.html", signin=False, login=False)
+    if session:
+        try:
+            connect = sqlite3.connect("data.db")
+            cursor = connect.cursor()
+            req = cursor.execute("SELECT * FROM messages")
+            messages_res = req.fetchall()
+        except Exception as e:
+            connect.rollback()
+            print(e)
+        finally:
+            connect.close()
+        return render_template("forum.html",messages = reversed(messages_res))
+    else:
+        return render_template("index.html", signin=False, login=False)
 
 @app.route("/signin", methods=["POST"])
 def signin():
@@ -38,7 +53,7 @@ def signin():
     finally:
         connect.close()
     
-    return render_template("forum.html")
+    return redirect("/")
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -62,13 +77,33 @@ def login():
     finally:
         connect.close()
     
-    return render_template("forum.html")
+    return redirect("/")
 
 
 @app.route("/clear")
 def clear():
     session.clear()
     return redirect("/")
+
+@app.route("/send")
+def send():
+    content = request.args.get("messagecontent")
+    author = session["user"][1]
+    time = "Le "+ datetime.datetime.now().strftime("%d %m %y")
+    token = secrets.token_hex(6)
+    try:
+        connect = sqlite3.connect("data.db")
+        cursor = connect.cursor()
+        req = cursor.execute("INSERT INTO messages (token, author, datetime, content) VALUES (?,?,?,?)", (token, author, time, content))
+        connect.commit()
+    except Exception as e:
+        connect.rollback()
+        print(e)
+    finally:
+        connect.close()
+        
+    return redirect(url_for("home_page"))
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
